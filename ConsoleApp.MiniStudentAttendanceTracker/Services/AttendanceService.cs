eb76a1faaa0763caefe0b7876e3e8fc7575baabe
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-03 17:26:43 +0630

    [Complete] Services

---
+++ ConsoleApp.MiniStudentAttendanceTracker/Services/AttendanceService.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp.MiniStudentAttendanceTracker;
+
+public class AttendanceService
+{
+    private readonly IStudentRepository _studentRepository;
+    private readonly IAttendanceRepository _attendanceRepository;
+
+    public AttendanceService(IStudentRepository studentRepository, IAttendanceRepository attendanceRepository)
+    {
+        _studentRepository = studentRepository;
+        _attendanceRepository = attendanceRepository;
+    }
+
+    public void MarkAttendance(int studentId, DateTime date, bool isPresent)
+    {
+        var student = _studentRepository.GetStudent(studentId);
+        if (student == null)
+        {
+            throw new ArgumentException("Student not found.");
+        }
+
+        var record = new AttendanceRecord
+        {
+            StudentId = studentId,
+            Date = date,
+            IsPresent = isPresent
+        };
+
+        _attendanceRepository.AddAttendanceRecord(record);
+    }
+
+    public IEnumerable<AttendanceRecord> GetAttendanceByStudent(int studentId)
+    {
+        return _attendanceRepository.GetAttendanceRecordsByStudent(studentId);
+    }
+}
