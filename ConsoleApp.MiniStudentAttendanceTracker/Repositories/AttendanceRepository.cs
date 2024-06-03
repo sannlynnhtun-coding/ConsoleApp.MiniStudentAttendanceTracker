8ec9102f497c6389b289301a698055b1d0fdad44
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-06-03 17:27:03 +0630

    [Complete] Repositories

---
+++ ConsoleApp.MiniStudentAttendanceTracker/Repositories/AttendanceRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.MiniStudentAttendanceTracker;
+
+public class AttendanceRepository : IAttendanceRepository
+{
+    private readonly List<AttendanceRecord> _attendanceRecords = new List<AttendanceRecord>();
+
+    public void AddAttendanceRecord(AttendanceRecord record)
+    {
+        _attendanceRecords.Add(record);
+    }
+
+    public IEnumerable<AttendanceRecord> GetAttendanceRecordsByStudent(int studentId)
+    {
+        return _attendanceRecords.Where(r => r.StudentId == studentId);
+    }
+}
---
+++ ConsoleApp.MiniStudentAttendanceTracker/Repositories/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp.MiniStudentAttendanceTracker;
+
+public class StudentRepository : IStudentRepository
+{
+    private readonly List<Student> _students = new List<Student>();
+
+    public void AddStudent(Student student)
+    {
+        _students.Add(student);
+    }
+
+    public Student GetStudent(int id)
+    {
+        return _students.FirstOrDefault(s => s.Id == id);
+    }
+
+    public IEnumerable<Student> GetAllStudents()
+    {
+        return _students;
+    }
+}
